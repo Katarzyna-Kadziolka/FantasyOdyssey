ef13150e763fc4480842f35c7e33c649f54d3fb2
Katarzyna <kasiakadziolka@gmail.com>
2023-04-20 12:14:22 +0200

    fixed google fit

--- lib/login_controller.dart
+++ lib/login_controller.dart
@@ -2,11 +2,10 @@ import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginController extends GetxController {
-  final _googleSignIn = GoogleSignIn();
+  final _googleSignIn = GoogleSignIn(scopes: ["https://www.googleapis.com/auth/fitness.activity.read"]);
   var googleAccount = Rx<GoogleSignInAccount?>(null);
 
   login() async {
     googleAccount.value = await _googleSignIn.signIn();
-
   }
 }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:fantasy_odyssey/login_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:health/health.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class LoginPage extends StatelessWidget {
   final controller = Get.put(LoginController());
@@ -52,7 +54,46 @@ class LoginPage extends StatelessWidget {
           controller.googleAccount.value?.email ?? '',
           style: Get.textTheme.bodySmall,
         ),
+        ElevatedButton(onPressed: _connectToGoogleFit, child: const Text('Connect to Google Fit'))
       ],
     );
+  }
+
+  void _connectToGoogleFit () async{
+    await Permission.activityRecognition.request();
+    // create a HealthFactory for use in the app
+    HealthFactory health = HealthFactory();
+
+    // define the types to get
+    var types = [
+      HealthDataType.STEPS,
+    ];
+
+    // requesting access to the data types before reading them
+    bool requested = await health.requestAuthorization(types);
+
+    var now = DateTime.now();
+    var steps2 = await health.getTotalStepsInInterval(now.subtract(Duration(days: 1)), now);
+
+
+    // fetch health data from the last 24 hours
+    List<HealthDataPoint> healthData = await health.getHealthDataFromTypes(
+        now.subtract(Duration(days: 1)), now, types);
+
+    // request permissions to write steps and blood glucose
+    types = [HealthDataType.STEPS, HealthDataType.BLOOD_GLUCOSE];
+    var permissions = [
+      HealthDataAccess.READ_WRITE,
+      HealthDataAccess.READ_WRITE
+    ];
+    await health.requestAuthorization(types, permissions: permissions);
+
+    // write steps and blood glucose
+    bool success = await health.writeHealthData(10, HealthDataType.STEPS, now, now);
+    success = await health.writeHealthData(3.1, HealthDataType.BLOOD_GLUCOSE, now, now);
+
+    // get the number of steps for today
+    var midnight = DateTime(now.year, now.month, now.day);
+    int? steps = await health.getTotalStepsInInterval(midnight, now);
   }
 }
