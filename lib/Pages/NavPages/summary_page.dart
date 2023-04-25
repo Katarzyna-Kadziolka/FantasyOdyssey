27e5a51704b943fa20148f4e716020692bd512f2
Katarzyna <kasiakadziolka@gmail.com>
2023-04-25 15:58:54 +0200

    Summary page text styles

--- lib/Pages/NavPages/summary_page.dart
+++ lib/Pages/NavPages/summary_page.dart
@@ -13,30 +13,58 @@ class _SummaryPageState extends State<SummaryPage> {
     return Container(
       constraints: const BoxConstraints.expand(),
       decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("images/BagEndToRivendel.png"),
-              fit: BoxFit.cover),
-          ),
+        image: DecorationImage(
+            image: AssetImage("images/BagEndToRivendel.png"),
+            fit: BoxFit.cover),
+      ),
       alignment: Alignment.bottomCenter,
       child: Container(
         height: 250,
         width: 250,
         margin: const EdgeInsets.only(bottom: 50),
-
         child: Card(
           color: const Color(0xFF00695C).withOpacity(0.5),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25)
+          ),
           child: DefaultTextStyle(
-            style: TextStyle(color: Color(0xFFE0F2F1)),
-            child: Column (
-              children: [
-                Text("text1"),
-                Text("text2")
-              ],
+            style: const TextStyle(color: Color(0xFFE0F2F1)),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "Bagend to Rivendel",
+                    style: TextStyle(fontSize: 22),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Today",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Text(
+                        "6,8 km",
+                        style: TextStyle(fontSize: 35),
+                      ),
+                      Text(
+                        "3200 steps",
+                        style: TextStyle(fontSize: 22),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "Total: 30 km (6%)",
+                    style: TextStyle(fontSize: 15),
+                  )
+                ],
+              ),
             ),
           ),
         ),
       ),
-
     );
   }
 }
