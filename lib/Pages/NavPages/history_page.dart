2f9163ba54299ed459f859eabd4818498fdc70c1
Katarzyna <kasiakadziolka@gmail.com>
2023-04-25 11:22:02 +0200

    Navigation start

---
+++ lib/Pages/NavPages/history_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HistoryPage extends StatelessWidget {
+  const HistoryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          "HistoryPage"
+        ),
+      )
+    );
+  }
+}
